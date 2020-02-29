0f54a454f4930d1a28462af327d9db9d27443f55
Olivia <21kane@friendsseminary.org>
2020-02-28 21:32:08 -0500

    Music App HW

--- musicApp/ViewController.swift
+++ musicApp/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonKeyPressed(_ sender: UIButton) {
+        playSound(label : sender.currentTitle!)
+    }
+    
+    func playSound(label : String) {
+        guard let url = Bundle.main.url(forResource: label, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    } //end sound
+    
+    
 }
 
